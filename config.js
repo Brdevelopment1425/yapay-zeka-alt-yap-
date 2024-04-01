f2d4b880aca686ec44ab4bce3127e247876a20dd
Ege-Mehti-Toremen <142041509+firma123485@users.noreply.github.com>
2024-04-01 19:31:50 +0300

    yapay-zeka-abone-sistemi
    vip alt yapısı

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    token: "MTIxODEzNjE3MjE3NDUxMjIwOQ.GW_mc1._KbSssUnh6cPdji07k7SKmspIsmCb4CGy90IX4", //bot tokeniniz
+    clientId: "1218136172174512209", //Bot idsi
+    key: "K85976261288957", //ocr.space key link : https://ocr.space
+
+    //Alttakileri elleme !
+    langs: [
+        'Subscribed',
+        'Aboneolundu',
+        'Abunəoldu',
+        'Abone olundu',
+        'Abunə oldu',
+        'Gezeichnet',
+        'Abonné',
+        'Εγγεγραμμένος'
+    ],
+}
